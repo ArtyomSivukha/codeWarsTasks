fa43d6f3490e216d10d28319496130ef96f6cb28
Artyom Sivukha <artyom.sivukha@gmail.com>
2022-08-29 20:06:51 +0300

    add replace script

---
+++ Complementary DNA.js
@@ -0,0 +1,9 @@
+function DNAStrand(dna) {
+    return dna
+        .replace(/A/g, 't')
+        .replace(/T/g, 'a')
+        .replace(/C/g, 'g')
+        .replace(/G/g, 'c')
+        .toUpperCase();
+}
+console.log(DNAStrand("CGAT"));
